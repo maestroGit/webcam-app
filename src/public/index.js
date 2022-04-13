41802e76d4fd7c5b0250e50d12409173ce96337a
walkexperience <walkexpereince@gmail.com>
2022-04-13 18:12:33 +0200

    Solved issue mediaDevice

--- src/public/index.js
+++ src/public/index.js
@@ -1,43 +1,42 @@
-'use strict'; 
+"use strict";
 // Para hacer que el código sea mas seguro.
 
 // Definimos las constantes que vamos a utilizar
-const videoFrame = document.getElementById('video_frame');
-const canvasFrame = document.getElementById('canvas_frame');
+const videoFrame = document.getElementById("video_frame");
+const canvasFrame = document.getElementById("canvas_frame");
 const snapFrame = document.getElementById("snap_frame");
-const errorMsgElement = document.querySelector('span#errorMsg');
+const errorMsgElement = document.getElementById("errorMsg");
 
 // Definimos tamaño del video y si queremos audio o no
 const constraints = {
-    audio: true,
-    video: {
-        width: 720, height: 405
-    }
+  audio: false,
+  video: true,
 };
-console.log(constraints);
+console.log({ constraints });
 
 // Comprobamos acceso a la Webcam
 async function init() {
-    try {
-        let stream = null;
-        stream = await navigator.mediaDevices.getUserMedia(constraints);
-        handleSuccess(stream);
-    } catch (e) {
-        errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
-    }
+  try {
+    let stream = null;
+    console.log({ stream });
+    stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
 }
 
 // En caso de que el acceso sea correcto, cargamos la webcam
 async function handleSuccess(stream) {
-    window.stream = stream;
-    videoFrame.srcObject = stream;
+  window.stream = stream;
+  videoFrame.srcObject = stream;
 }
 
 // Iniciamos JS
 init();
 
 // Hacemos captura de pantalla al hacer click
-var context = canvasFrame.getContext('2d');
-snapFrame.addEventListener("click", function() {
-    context.drawImage(videoFrame, 0, 0, 320, 140);
+var context = canvasFrame.getContext("2d");
+snapFrame.addEventListener("click", function () {
+  context.drawImage(videoFrame, 0, 0, 320, 140);
 });
